dd7ef9aeeeeec2a546a7e2dc5599f9b1bc93ef18
nodoors <nodoors07007@gmail.com>
2013-06-23 14:49:20 +0800

    monster image

--- src/BadMonster.java
+++ src/BadMonster.java
@@ -1,4 +1,29 @@
-
 public class BadMonster extends Monster {
+	private final int cold_down;
+	private int cold_down_now;
+
+	public BadMonster(String name, int blood, int attack_power,
+			int recover_power, JujuType monster_type, String imageDir,
+			int cold_down) {
+		super(name, blood, attack_power, recover_power, monster_type, imageDir);
+		this.cold_down = cold_down;
+		cold_down_now = cold_down;
+	}
+
+	public int get_cold_down() {
+		return cold_down;
+	}
+
+	public int get_cold_down_now() {
+		return cold_down_now;
+	}
+
+	public void Round_cold_down() {// use get_cold_down() to decide Attack or
+									// not. Round_cold_down last
+		if (cold_down_now > 1)
+			cold_down_now = cold_down_now - 1;
+		else if (cold_down_now == 1)
+			cold_down_now = cold_down;
+	}
 
 }
